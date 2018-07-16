33be3f04a9af83731a5223a8802b363d0a47ad3a
Alexander Tiderko <Alexander.Tiderko@gmail.com>
2018-07-16 18:32:10 +0200

    Rebase from 'upstream'

--- node_manager_fkie/src/node_manager_fkie/start_handler.py
+++ node_manager_fkie/src/node_manager_fkie/start_handler.py
@@ -554,7 +554,10 @@ class StartHandler(object):
                         rospy.logwarn("ERROR while start '%s': %s", name, error)
                         raise StartException(''.join(['The host "', host, '" reports:\n', error]))
                     if output:
-                        rospy.logdebug("STDOUT while start '%s': %s", name, output)
+                        if output.find("dn't") != -1:
+                            rospy.logwarn("Warning while start '%s': %s", name, output)
+                        else:
+                            rospy.loginfo("STDOUT while start '%s': %s", name, output)
                 else:
                     if error:
                         rospy.logwarn("ERROR while start '%s': %s", name, error)
@@ -806,7 +809,10 @@ class StartHandler(object):
         else:
             try:
                 # output ignored: output, error, ok
-                nm.ssh().ssh_exec(host, [nm.settings().start_remote_script, '--delete_logs', nodename], user, pw, auto_pw_request, close_stdin=True, close_stdout=True, close_stderr=True)
+                _, stdout, _, ok = nm.ssh().ssh_exec(host, [nm.settings().start_remote_script, '--delete_logs', nodename], user, pw, auto_pw_request, close_stdin=True, close_stdout=False, close_stderr=True)
+                if ok:
+                    stdout.readlines()
+                    stdout.close()
             except nm.AuthenticationRequest as e:
                 raise nm.InteractionNeededError(e, cls.deleteLog, (nodename, host, auto_pw_request))
 
