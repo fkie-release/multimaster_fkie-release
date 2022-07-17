a5217edffc310b0ac77b4e0da0a4efd44ab0000f
Alexander Tiderko <alexander.tiderko@fkie.fraunhofer.de>
2022-07-17 22:50:29 +0200

    Trimmed the branch to only the fkie_multimaster_msgs sub directory

--- run_tests.sh
+++
@@ -1,2 +0,0 @@
-nosetests -s -vv -w fkie_master_discovery/tests/ --with-coverage --cover-erase --nocapture --cover-package=fkie_master_discovery
-nosetests -s -vv -w fkie_node_manager_daemon/tests/ --with-coverage --cover-erase --nocapture --cover-package=fkie_node_manager_daemon
